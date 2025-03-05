dacb34575f9929e53bea2b27378b1bf2752f9dd1
CH-Abdul-Hadi <adbulhadi502@gmail.com>
2025-03-05 22:57:23 +0500

    hero section complete

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import style from './Button.module.css'
 
 function Button(props) {
+// function Button(props) {
   return (
     <>
-      <button className={style.btns}>{props.btn}</button>
+      <button className={style.btns} style={props.btnstyle} >{props.btn}</button>
+      {/* <button className={props.btnstyle}>{props.btn}</button> */}
     </>
   )
 }
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
-import style from './Hero.module.css'
+import React from "react";
+import style from "./Hero.module.css";
+import profile from "../../assets/Profile_img.jpg";
+import Button from "../Button/Button";
+
 
 function Hero() {
   return (
     <>
-      
+    <div className={style.hero}>
+      <img src={profile} alt="" className={style.profileImg}  />
+      <h1>I'm Front-End-Developer based in Pakistan </h1>
+      <p>
+        I have word with Professionals and currently doing MERN stack from
+        Saylani Mass IT Training Center Peshawar
+      </p>
+      <div className={style.herobtns} >
+      <Button btn='Connect with me' />
+      <Button btn="My Resume"  />
+      {/* <Button btn='My Resume' btnstyle={{backgroundColor:"black"}} /> */}
+      </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
