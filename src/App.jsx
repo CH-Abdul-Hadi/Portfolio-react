8a0f5e0dde89781ddea9b4f8ce1ffffc63d744f8
CH-Abdul-Hadi <adbulhadi502@gmail.com>
2025-03-15 11:59:54 +0500

    gsap added

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,7 @@ import AboutMe from "./components/AboutMe/AboutMe";
 import Projects from "./components/Projects/Projects";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
-// import Slider from './components/Slider/Slider'
-
+// import { gsap } from "gsap";
 function App() {
   return (
     <>
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,26 +1,46 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import style from "./Hero.module.css";
 import profile from "../../assets/Profile_img.webp";
 import Button from "../Button/Button";
-import styleTextColor from '../Contact/Contact.module.css'
+import styleTextColor from "../Contact/Contact.module.css";
+import { gsap } from "gsap";
+import { TextPlugin } from "gsap/TextPlugin";
 
+gsap.registerPlugin(TextPlugin);
 
 function Hero() {
+  const imgRef = useRef(null);
+  const textRef = useRef(null);
+  const paraRef = useRef(null);
+  const btnsRef = useRef(null);
+
+  useEffect(() => {
+    gsap.from(textRef.current, {
+      text: "Front-End-Developer",
+      duration: 6,
+      ease: "none",
+      delay: 0.5,
+    });
+  }, []);
+
   return (
-    <>
-    <div className={style.hero} >
-      <img src={profile} alt="" className={style.profileImg}  />
-      <h1>I'm <span className={styleTextColor.contactGet}>Front-End-Developer </span> based in Pakistan </h1>
-      <p>
-        I have word with Professionals and currently doing MERN stack from
+    <div className={style.hero}>
+      <img ref={imgRef} src={profile} alt="" className={style.profileImg} />
+      <h1>
+        I'm <span ref={textRef} className={styleTextColor.contactGet}></span>
+        based in Pakistan{" "}
+      </h1>
+      <p ref={paraRef}>
+        I have worked with Professionals and currently doing MERN stack from
         Saylani Mass IT Training Center Peshawar
       </p>
-      <div className={style.herobtns} >
-      <a href="#contact"><Button btn='Contact Me'  /></a>
-      <Button btn="My Resume"  />
+      <div ref={btnsRef} className={style.herobtns}>
+        <a href="#contact">
+          <Button btn="Contact Me" />
+        </a>
+        <Button btn="My Resume" />
       </div>
-      </div>
-    </>
+    </div>
   );
 }
 
